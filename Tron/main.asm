128e83c591661b210c85d852b884c7b370119b84
adrianosabo <adrianosabo@mac-mini-de-adriano.local>
2018-08-01 00:04:25 -0300

    added collision

--- Tron/main.asm
+++ Tron/main.asm
@@ -33,8 +33,9 @@
         include "vcs.h"
         include "macro.h"
         include "xmacro.h"
-
-    
+ 
+SpriteHeight	equ 17 
+ 
 ;===============================================================================
 ; Define RAM Usage
 ;===============================================================================
@@ -53,18 +54,21 @@ Player1Y	.byte
 Player2X	.byte
 Player2Y	.byte      
 
+Player1YPrev	.byte
+Player1XPrev	.byte
 
-PF0_left	ds 20
-PF1_left	ds 20
-PF2_left	ds 20
-PF0_right	ds 20
-PF1_right	ds 20
-PF2_right	ds 20
+Player2YPrev	.byte
+Player2XPrev	.byte
+
+PF0_left	ds 19
+PF1_left	ds 19
+PF2_left	ds 19
+PF0_right	ds 19
+PF1_right	ds 19
+PF2_right	ds 19
 
 ;Player1Score	.byte
 ;Player2core	.byte            
-
-
 	       
 ;===============================================================================
 ; Define Start of Cartridge
@@ -85,14 +89,11 @@ InitSystem:
     	; CLEAN_START is a macro found in macro.h
     	; it sets all RAM, TIA registers and CPU registers to 0
         CLEAN_START   
-
-        ;lda #160
-        ;sta PF0_left,1
         
         ;player position
         lda #0
         sta Player1X
-        lda #5
+	lda #0
         sta Player1Y
         
         ;player position
@@ -101,10 +102,10 @@ InitSystem:
         lda #15
         sta Player2Y        
 
-        ldy #120
-        lda #0
-initGrid:        
-        sta PF0_left,y	
+        ldy #144
+        lda #$00
+initGrid:
+	sta PF0_left,y	
         dey
         bne initGrid        
         
@@ -114,7 +115,7 @@ initGrid:
 
 Main:
         jsr VerticalSync    ; Jump to SubRoutine VerticalSync
-        jsr VerticalBlank   ; Jump to SubRoutine VerticalBlank
+	jsr VerticalBlank   ; Jump to SubRoutine VerticalBlank
         jsr Kernel          ; Jump to SubRoutine Kernel
         jsr OverScan        ; Jump to SubRoutine OverScan
         jmp Main            ; JuMP to Main
@@ -137,7 +138,7 @@ VerticalSync:
         sta WSYNC   ; Wait for Sync - halts CPU until end of 1st scanline of VSYNC
         sta WSYNC   ; wait until end of 2nd scanline of VSYNC
         lda #0      ; LoaD Accumulator with 0 so D1=0
-        sta WSYNC   ; wait until end of 3rd scanline of VSYNC
+	sta WSYNC   ; wait until end of 3rd scanline of VSYNC
         sta VSYNC   ; Accumulator D1=0, turns off Vertical Sync signal
         rts         ; ReTurn from Subroutine
     
@@ -164,10 +165,10 @@ Kernel:
         bne Kernel      ; Branch if its Not Equal to 0
     	; turn on the display
         sta VBLANK      ; Accumulator D1=0, turns off Vertical Blank signal (image output on)
-        
-		; Set up timer (in case of bugs where we don't hit exactly)
-		TIMER_SETUP 192
-		SLEEP 10 ; to make timing analysis work out
+
+        ; Set up timer (in case of bugs where we don't hit exactly)
+        TIMER_SETUP 192
+        SLEEP 10 ; to make timing analysis work out
        
         ;from the beging over and over
         sta WSYNC
@@ -179,13 +180,31 @@ Kernel:
         lda #$70;ColorBackGround,y
         sta COLUBK
         
-		lda #$48;ColorForeGround,y
+	lda #$48;ColorForeGround,y
         sta COLUPF
                        
+                  
+	;---- MISSILE POSITION
+        lda #$52
+        sta COLUP0
+        lda #2; bit 1 now set         
+        sta ENAM0; enable/disable missile
+        
+        lda #$A0
+        sta HMM0
+        ;sta RESMP0
+        
+        lda #$A2
+        sta COLUP1
+        lda #2; bit 1 now set 
+        sta ENAM1; enable/disable missile        
+        
 RowsHeightLoop
 
-		sta WSYNC        
+	sta WSYNC        
+	
 
+        
 PatternChanged
 
         lda PF0_left,y 
@@ -197,20 +216,21 @@ PatternChanged
         lda PF2_left,y 
         sta PF2 ; PF2
                
-		SLEEP 8
+	SLEEP 8
         
         lda PF0_right,y
         sta PF0 ; PF0 
         
         lda PF1_right,y
-        sta PF1 ; PF1    
+        sta PF1 ; PF1    33aqq
         
         lda PF2_right,y
         sta PF2 ; PF2        
         
         dex
         bne RowsHeightLoop  ; Branch if Not Equal to 0    
-		SLEEP 8; == nop X 6
+	SLEEP 8; == nop X 6
+   
         
         iny ;next pattern index
       	lda LineHeight,y
@@ -220,75 +240,93 @@ PatternChanged
         jmp PatternChanged;RowsHeightLoop; == 0, we're done
         
 RowsEnd        
-		sta WSYNC
-		
-		lda #0
-		sta PF0
-		sta PF1
-		sta PF2 ; clear playfield
-		sta COLUBK
-		sta COLUPF  
+        sta WSYNC
 
-		; Wait for timer to finish
-		TIMER_WAIT
+        lda #0
+        sta PF0
+        sta PF1
+        sta PF2 ; clear playfield
+        sta COLUBK
+        sta COLUPF  
+
+        ; Wait for timer to finish
+        TIMER_WAIT
         rts	; ReTurn from Subroutine
 
+CheckColliison subroutine
+        ;check collisions
+; Did the player collide with the wall?
+	bit CXP0FB
+        bpl NoCollision
+; Yes, load previous position
+        lda Player1YPrev
+        sta Player1Y
+        lda Player1XPrev
+        sta Player1X
+        jmp NoMoveJoy
+NoCollision
+; No collision, update previous position and move player
+        lda Player1Y
+        sta Player1YPrev
+        lda Player1X
+        sta Player1XPrev
+        jsr MoveJoystick
+NoMoveJoy     
+	rts
+
 UpdateGrid subroutine
-		tya
+	tya
         lsr
         lsr
         tay
                 
-		txa	
-		lsr
-		lsr ; div 4
-	
-		cmp #20
-		bpl SecondHalf
+	txa	
+	lsr
+	lsr ; div 4
+
+        cmp #20
+        bpl SecondHalf
 firstHalf:
 		
 pf0_l:	
-		cmp #4
-		bpl pf1_l
-		tax
-		lda PF0_left,y
-		ora BitReprF0,x		
-		sta PF0_left,y		
-		rts 
-pf1_l: 
-
-		cmp #12
-		bpl pf2_l
-		sec
-		sbc #4
-		tax
-		lda PF1_left,y
-		ora BitReprF1,x		
-		sta PF1_left,y	
-		rts
-pf2_l: 
-
-		sec
-		sbc #12
-		tax
-		lda PF2_left,y	
-		ora BitReprF2,x		
-		sta PF2_left,y	
-		rts
+        cmp #4
+        bpl pf1_l
+        tax
+        lda PF0_left,y
+        ora BitReprF0,x		
+        sta PF0_left,y		
+        rts 
+pf1_l:
+        cmp #12
+        bpl pf2_l
+        sec
+        sbc #4
+        tax
+        lda PF1_left,y
+        ora BitReprF1,x		
+        sta PF1_left,y	
+        rts
+pf2_l:
+        sec
+        sbc #12
+        tax
+        lda PF2_left,y	
+        ora BitReprF2,x		
+        sta PF2_left,y	
+        rts
 		
 SecondHalf: 
-		sbc #20
+	sbc #20
 		
 pf0_r:	
-		cmp #4
-		bpl pf1_r
-		tax
-		lda PF0_right,y
-		ora BitReprF0,x		
-		sta PF0_right,y		
-		rts 
-pf1_r: 
-		
+        cmp #4
+        bpl pf1_r
+        tax
+        lda PF0_right,y
+        ora BitReprF0,x		
+        sta PF0_right,y		
+        rts 
+pf1_r:	
         cmp #12
         bpl pf2_r
         sec
@@ -305,44 +343,46 @@ pf2_r:
         lda PF2_right,y	
         ora BitReprF2,x		
         sta PF2_right,y	
-		rts	; Read joystick movement and apply to object 0
+	rts	
+        
+        ; Read joystick movement and apply to object 0
 MoveJoystick
-		; Move vertically
-		; (up and down are actually reversed since ypos starts at bottom)
-		ldx Player1Y
-		lda #%00010000	;Up?
-		bit SWCHA
-		bne SkipMoveUp
+        ; Move vertically
+        ; (up and down are actually reversed since ypos starts at bottom)
+        ldx Player1Y
+        lda #%00010000	;Up?
+        bit SWCHA
+        bne SkipMoveUp
         cpx #1
         bcc SkipMoveUp
         dex
 SkipMoveUp
-		lda #%00100000	;Down?
-		bit SWCHA 
-		bne SkipMoveDown
-        cpx #75
+        lda #%00100000	;Down?
+        bit SWCHA 
+        bne SkipMoveDown
+        cpx #74
         bcs SkipMoveDown
         inx
 SkipMoveDown
-		stx Player1Y
-		;Move horizontally
+	stx Player1Y
+	;Move horizontally
         ldx Player1X
-		lda #%01000000	;Left?
-		bit SWCHA
-		bne SkipMoveLeft
+	lda #%01000000	;Left?
+	bit SWCHA
+	bne SkipMoveLeft
         cpx #1
         bcc SkipMoveLeft
         dex
 SkipMoveLeft
-		lda #%10000000	;Right?
-		bit SWCHA 
-		bne SkipMoveRight
-        cpx #159
-        bcs SkipMoveRight
-        inx
+	lda #%10000000	;Right?
+	bit SWCHA 
+	bne SkipMoveRight
+	cpx #159
+	bcs SkipMoveRight
+	inx
 SkipMoveRight
-		stx Player1X
-		rts
+	stx Player1X
+	rts
         
 ;===============================================================================
 ; Overscan
@@ -352,34 +392,36 @@ SkipMoveRight
 ;===============================================================================
 
 OverScan:
-        sta WSYNC   ; Wait for SYNC (halts CPU until end of scanline)
+	sta WSYNC   ; Wait for SYNC (halts CPU until end of scanline)
         lda #2      ; LoaD Accumulator with 2 so D1=1
         sta VBLANK  ; STore Accumulator to VBLANK, D1=1 turns image output off
-        
-		; set the timer for 27 scanlines.  Each scanline lasts 76 cycles,
-		; but the timer counts down once every 64 cycles, so use this
-		; formula to figure out the value to set.  
-		;       (scanlines * 76) / 64    
-		; Also note that it might be slight off due to when on the scanline TIM64T
-		; is updated.  So use Stella to check how many scanlines the code is
-		; generating and adjust accordingly.
+
+        ; set the timer for 27 scanlines.  Each scanline lasts 76 cycles,
+        ; but the timer counts down once every 64 cycles, so use this
+        ; formula to figure out the value to set.  
+        ;       (scanlines * 76) / 64    
+        ; Also note that it might be slight off due to when on the scanline TIM64T
+        ; is updated.  So use Stella to check how many scanlines the code is
+        ; generating and adjust accordingly.
         lda #32     ; set timer for 27 scanlines, 32 = ((27 * 76) / 64)
         sta TIM64T  ; set timer to go off in 27 scanlines
-        
-    	; game logic will go here
 
-		;draw player 1
-        ldy Player1Y	
+        ; game logic will go here
+        ;jsr CheckColliison
+   
+
+        ;draw player 1
+	ldy Player1Y	
         ldx Player1X
         jsr UpdateGrid                
         ;inc Player1X
 
-		;draw player 2
-		ldy Player2Y	
+        ;draw player 2
+        ldy Player2Y	
         ldx Player2X
         jsr UpdateGrid                
         dec Player2X
-        
+
         jsr MoveJoystick
 OSwait:
         sta WSYNC   ; Wait for SYNC (halts CPU until end of scanline)
@@ -414,6 +456,24 @@ BitReprF1
 BitReprF2
 	.byte #%00000001,#%00000010,#%00000100,#%00001000,#%00010000,#%00100000,#%01000000,#%10000000
 
+Frame0
+	.byte #0
+        .byte #%00011000;
+        .byte #%00111100;
+        .byte #%00111100;
+        .byte #%01111110;
+        .byte #%00111100;
+        .byte #%00011000;
+        .byte #%00011000;
+        .byte #%10011001;
+        .byte #%10011001;
+        .byte #%00111100;
+        .byte #%00111100;
+        .byte #%00111100;
+        .byte #%00011000;
+        .byte #%00011000;
+        .byte #%00111100;
+        .byte #%00011000; 
 	          
 ;===============================================================================
 ; free space check before End of Cartridge
